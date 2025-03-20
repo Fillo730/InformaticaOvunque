fefd17b7cca6b1012221cc8dfdceb417223fbd66
Fillo730 <filloinformatica@gmail.com>
2025-03-20 09:30:34 +0100

    feat: implemented contactPage and ramPage

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -18,7 +18,7 @@ function Blog() {
               imageURL="https://i0.wp.com/semiengineering.com/wp-content/uploads/2018/09/Screen-Shot-2017-04-26-at-1.08.57-PM.png?ssl=1"/>
           <Article id="2" title="Come funziona il processore (CPU)" path="/blog/cpu"
               imageURL="https://cdni.iconscout.com/illustration/premium/thumb/cpu-processor-chip-illustration-download-in-svg-png-gif-file-formats--gpu-science-technology-illustrations-2862658.png"/>
-          <Article id="3" title="Pipeline CPU" path="/blog/pipeline-cpu"
+          <Article id="3" title="Pipeline CPU" path="/blog/cpu-pipeline"
               imageURL="https://microchip.wdfiles.com/local--files/32bit:mx-arch-pipeline/pic32mx-pipelined-execution.png"/>
           <Article id="4" title="Memoria principale (RAM)" path="/blog/pipeline-cpu"
               imageURL="https://it.crucial.com/content/dam/crucial/dram-products/ddr5-pro/overclocking/photography/in-use/crucial-ddr5-pro-overclocking-inuse-9.psd.transform/small-png/img.png"/>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,34 +1,38 @@
-import './Contact.css'
+import './Contact.css';
+import { Mail, Phone } from 'lucide-react';
 
-function Contact () {
+function Contact() {
   return (
     <div>
-        <div className='div-title'>
-          <h1>Chi sono io? Perchè scrivo cose sui computer?</h1>
-        </div>
-        <div className='div-text'>
-          <p className='p-text'>
-            Ciao, mi chiamo Filippo, sono uno studente di ingegneria informatica; da quando sono piccolo sono sempre stato appassionato
-            di computer ed elettronica in generale. Per questo motivo, ho deciso di fare questo piccolo blog; per divertermi e magari, far
-            imparare qualcosa di nuovo alle persone sfortunate che dovessero leggerlo. 
-          </p>
+      <div className='div-title'>
+        <h1>Chi sono io? Perché scrivo cose sui computer?</h1>
+      </div>
+      <div className='div-text'>
+        <p className='p-text'>
+          Ciao, mi chiamo Filippo, sono uno studente di ingegneria informatica; da quando sono piccolo sono sempre stato appassionato
+          di computer ed elettronica in generale. Per questo motivo, ho deciso di fare questo piccolo blog; per divertirmi e magari, far
+          imparare qualcosa di nuovo alle persone sfortunate che dovessero leggerlo.
+        </p>
 
-          <h3 className='h3-title'>Perchè proprio l'informatica?</h3>
-          <p className='p-text'>
-            Diciamo che sono di parte (sono un ingegnere informatico), ma comunque l'informatica ha qualcosa di particolare rispetto a tanti altri
-            campi. La usiamo tutti quanti, ne siamo completamente circondati ovunque giriamo lo sguardo, ma comunque la maggior parte di noi non
-            ha nemmeno la più pallida idea di cosa c'è "sotto il cofano". Ecco, magari girando un pò in questo blog te ne farai un idea e, chissà, 
-            magari ti innamorerai di questa meraviglia.
-          </p>
+        <h3 className='h3-title'>Perché proprio l'informatica?</h3>
+        <p className='p-text'>
+          Diciamo che sono di parte (sono un ingegnere informatico), ma comunque l'informatica ha qualcosa di particolare rispetto a tanti altri
+          campi. La usiamo tutti quanti, ne siamo completamente circondati ovunque giriamo lo sguardo, ma comunque la maggior parte di noi non
+          ha nemmeno la più pallida idea di cosa c'è "sotto il cofano". Ecco, magari girando un po' in questo blog te ne farai un'idea e, chissà,
+          magari ti innamorerai di questa meraviglia.
+        </p>
 
-          <h3 className='h3-title'>Chiedimi Qualcosa</h3>
-          <p className='p-text'>
-            In caso vuoi contattarmi per qualsiasi motivo, anche due chiacchere, scrivimi pure alla seguente email:<br/>
-            <strong>Email: </strong> mondoinformatico@gmail.com
-          </p>
-        </div>
+        <h3 className='h3-title'>Chiedimi Qualcosa</h3>
+        <p className='p-text'>
+          In caso vuoi contattarmi per qualsiasi motivo, anche due chiacchiere, scrivimi pure alla seguente email:<br/>
+          <strong><Mail size={20} className="icon" /> Email:</strong> mondoinformatico@gmail.com
+        </p>
+        <p className='p-text'>
+          <strong><Phone size={20} className="icon" /> Telefono:</strong> +39 123 456 7890
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Contact;

30e59be24eea6e6dd12dd5c8047f37f6cd097ae7
Fillo730 <filloinformatica@gmail.com>
2025-03-25 14:37:53 +0100

    refactor: removed useless tabs (for now)

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -12,8 +12,6 @@ function Blog() {
       <div className='tabs-container text-central'>
         <button className='tabs-button' onClick={() => setActiveTab('concettiFondamentali')}>Concetti Fondamentali</button>
         <button className='tabs-button' onClick={() => setActiveTab('baseArchitecture')}>Componenti di un Computer</button>
-        <button className='tabs-button' onClick={() => setActiveTab('operativeSystems')}>Sistemi Operativi</button>
-        <button className='tabs-button' onClick={() => setActiveTab('Internet')}>Internet</button>
       </div>
 
       {activeTab === "concettiFondamentali" && 
@@ -29,18 +27,6 @@ function Blog() {
           {componentArchitectureArticles.map(article => (
             <Article key={article.id} {...article} />
           ))}
-        </div>
-      }
-
-      {activeTab === "operativeSystems" && 
-        <div className='div-articles'>
-          
-        </div>
-      }
-
-      {activeTab === "Internet" && 
-        <div className='div-articles'>
-          
         </div>
       }
     </>

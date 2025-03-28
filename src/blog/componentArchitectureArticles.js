405a8cd20263782e2098ac39104c7ede3488cd53
Fillo730 <filloinformatica@gmail.com>
2025-03-28 23:24:45 +0100

    refactoring: refactored all pages

--- src/blog/componentArchitectureArticles.js
+++ src/blog/componentArchitectureArticles.js
@@ -2,37 +2,37 @@ const componentArchitectureArticles = [
   {
     id: "1",
     title: "Architettura base",
-    path: "/blog/componenti-computer/architettura-base",
+    path: "/blog/componenti-computer/1-architettura-base",
     text: "Chi è Von Neuman? Qual'è l'architettura base di un computer?"
   },
   {
     id: "2",
     title: "Processore (CPU)",
-    path: "/blog/componenti-computer/cpu",
+    path: "/blog/componenti-computer/2-cpu",
     text: "Cos'è la CPU? Perchè è indispensabile e cosa fa?"
   },
   {
     id: "3",
     title: "Pipeline CPU (avanzato)",
-    path: "/blog/componenti-computer/cpu-pipeline",
+    path: "/blog/componenti-computer/3-cpu-pipeline",
     text: "Come massimizzare le istruzioni eseguite ad ogni clock?"
   },
   {
     id: "4",
     title: "Memoria principale (RAM)",
-    path: "/blog/componenti-computer/ram",
+    path: "/blog/componenti-computer/4-ram",
     text: "Cos'è la RAM? Perchè è fondamentale per la velocità del computer?"
   },
   {
     id: "5",
     title: "Memorie (HDD/SSD)",
-    path: "/blog/componenti-computer/memoria-secondaria",
+    path: "/blog/componenti-computer/5-memoria-secondaria",
     text: "Come funziona la memoria secondaria? Cosa sono gli HDD e gli SSD?"
   },
   {
     id: "6",
     title: "Bus di sistema",
-    path: "/blog/componenti-computer/bus-di-sistema",
+    path: "/blog/componenti-computer/6-bus-di-sistema",
     text: "Come comunicano i componenti di un computer?"
   },    
 ];
---
+++ src/layouts/layoutsDependencies.js
@@ -0,0 +1,4 @@
+import HomeLayout from "./baseLayout/HomeLayout";
+import HtmlLayout from "./htmlLayout/HtmlLayout.astro";
+
+export {HomeLayout, HtmlLayout};

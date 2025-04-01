e4d59c0de8856a74816504bca0a0e90123703ac6
Fillo730 <filloinformatica@gmail.com>
2025-04-01 22:15:01 +0200

    styles: improved header style

--- src/layouts/baseLayout/Header.jsx
+++ src/layouts/baseLayout/Header.jsx
@@ -5,14 +5,14 @@ function Header() {
     return(
         <div className="div-container primary-gradient-right-left">
             <nav className="flex-central">
-                <ul className='ul'>
-                    <li className='ul-li'>
+                <ul className='ul '>
+                    <li className='ul-li text-pop-up-top'>
                         <a href='/home' className='primary-color ul-li-a'>Home</a>
                     </li>
-                    <li>
+                    <li className='ul-li text-pop-up-top'>
                         <a href='/contact' className='primary-color ul-li-a'>Contattami</a>
                     </li>
-                    <li>
+                    <li className='ul-li text-pop-up-top'>
                         <a href='/blog' className='primary-color ul-li-a'>Blog</a>
                     </li>
                 </ul>
